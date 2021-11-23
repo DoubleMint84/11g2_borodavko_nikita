df840c388cfb8b10f2fe01866f7c635b06e07f37
Nikita Borodavko <nikitka.borodavko@gmail.com>
2021-11-23 23:24:39 +0300

    Добавлены SQL-запросы

---
+++ query.sql
@@ -0,0 +1,9 @@
+UPDATE users SET birthdate = SUBSTR(birthdate, 7, 4) || '-' || SUBSTR(birthdate, 4, 2) || '-' || SUBSTR(birthdate, 1, 2);
+
+SELECT * FROM users ORDER BY id DESC LIMIT 1;
+
+SELECT DISTINCT(SUBSTR(birthdate, 1, 4)) as unique_years FROM users;
+
+SELECT COUNT(*) as total_items FROM items;
+
+SELECT avg((julianday('now') - julianday(birthdate)) / 365) FROM users WHERE (strftime('%m','now') - strftime('%m',registration_date)) <= 2;
