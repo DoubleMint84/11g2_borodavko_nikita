1a7050f82bf98cf2c1b7ec3b73b1c0fade9caa35
Nikita Borodavko <nikitka.borodavko@gmail.com>
2021-11-24 09:09:09 +0300

    Исправлен запрос

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 UPDATE users SET birthdate = SUBSTR(birthdate, 7, 4) || '-' || SUBSTR(birthdate, 4, 2) || '-' || SUBSTR(birthdate, 1, 2);
 
-SELECT * FROM users ORDER BY id DESC LIMIT 1;
+SELECT "login" FROM users ORDER BY id DESC LIMIT 1;
 
 SELECT DISTINCT(SUBSTR(birthdate, 1, 4)) as unique_years FROM users;
 
