53cbdb9b8a0e330e56b06a30856df5ea557b1de7
Nikita Borodavko <nikitka.borodavko@gmail.com>
2022-03-20 22:36:03 +0300

    fixed bug

--- app/app.py
+++ app/app.py
@@ -3,7 +3,4 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///extrud3r_orm.db'
-app.secret_key = 'secret'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
